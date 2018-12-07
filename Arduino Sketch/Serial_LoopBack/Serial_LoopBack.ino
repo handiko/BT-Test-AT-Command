451faac0ad0b43c68a6b36042244770808d5dc83
handiko <handikogesang@gmail.com>
2018-12-07 09:23:39 +0700

    Adding Serial BT

--- Arduino Sketch/Serial_LoopBack/Serial_LoopBack.ino
+++ Arduino Sketch/Serial_LoopBack/Serial_LoopBack.ino
@@ -1,12 +1,14 @@
+//
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(4800);
+  Serial.begin(1200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available())
   {
-    Serial.println(Serial.read(),DEC);
+    Serial.println(Serial.read());
   }
 }
